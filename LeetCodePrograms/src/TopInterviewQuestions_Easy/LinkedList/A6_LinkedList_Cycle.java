183a054b424188721f8165137079d73493aef557
rajeshk738 <kandimalla.rajesh2010@gmail.com>
2021-05-22 23:18:44 -0400

    A6

---
+++ LeetCodePrograms/src/TopInterviewQuestions_Easy/LinkedList/A6_LinkedList_Cycle.java
@@ -0,0 +1,44 @@
+package TopInterviewQuestions_Easy.LinkedList;
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        
+        if(head == null || head.next == null)
+            return false;
+        
+        ListNode fast = head.next;
+        ListNode slow = head;
+        
+        while(slow != fast){
+            
+            if(fast == null || fast.next == null)
+                return false;
+           
+            slow = slow.next;
+            fast = fast.next.next;
+            }
+        
+        
+        return true;        
+    }
+}
+
+
+public class A6_LinkedList_Cycle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
