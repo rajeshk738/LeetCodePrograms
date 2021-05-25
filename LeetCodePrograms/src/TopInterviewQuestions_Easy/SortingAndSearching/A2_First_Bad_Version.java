35ac28f60c5fc1a618d97a14c03465521bc532d2
rajeshk738 <kandimalla.rajesh2010@gmail.com>
2021-05-25 18:18:25 -0400

    A2

---
+++ LeetCodePrograms/src/TopInterviewQuestions_Easy/SortingAndSearching/A2_First_Bad_Version.java
@@ -0,0 +1,37 @@
+package TopInterviewQuestions_Easy.SortingAndSearching;
+
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+boolean isBadVersion(int version); */
+
+public class Solution2 extends VersionControl {
+public int firstBadVersion(int n) {
+
+int low = 1, high = n;
+
+while(low < high){
+    int mid = low + (high - low) / 2;
+            
+    if(isBadVersion(mid)){
+        high = mid;
+    }
+    else{
+        low = mid + 1;
+    }
+    
+}
+
+
+return low;
+}
+}
+
+
+public class A2_First_Bad_Version {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
