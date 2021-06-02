85ee20d4aa11f1bbe8d84c8707824744e6670177
rajeshk738 <kandimalla.rajesh2010@gmail.com>
2021-06-01 22:01:58 -0400

    A3

---
+++ LeetCodePrograms/src/TopInterviewQuestions_Easy/Trees/A3_Symmetric_Tree.java
@@ -0,0 +1,39 @@
+package TopInterviewQuestions_Easy.Trees;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution3 {
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root, root);
+    }
+    private boolean isMirror(TreeNode t1, TreeNode t2){
+        if(t1 == null && t2 == null)
+            return true;
+        if(t1 == null || t2 == null)
+            return false;
+        
+        return (t1.val == t2.val) && isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
+    }
+}
+
+public class A3_Symmetric_Tree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
