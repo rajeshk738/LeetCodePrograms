8929057422caca62139ac52cb0cc6bf2c0f99f36
rajeshk738 <kandimalla.rajesh2010@gmail.com>
2021-06-05 14:42:55 -0400

    A4

--- LeetCodePrograms/src/TopInterviewQuestions_Easy/DynamicProgramming/A4_House_Robber.java
+++ LeetCodePrograms/src/TopInterviewQuestions_Easy/DynamicProgramming/A4_House_Robber.java
@@ -1,20 +1,26 @@
 package TopInterviewQuestions_Easy.DynamicProgramming;
 
 class Solution4 {
-    public int rob(int[] nums) {
-        if(nums.length == 1)
-        	return nums[0];
-        
-        if(nums.length == 2){
-            if(nums[0] > nums[1])
-                return nums[0];
-            else 
-                return nums[1];
-        }
-        
+	    public int rob(int[] nums) {
+	        if(nums.length == 1)
+	        	return nums[0];
+	        
+	        if(nums == null || nums.length == 0)
+	            return 0;
+	        
+	        int robNext = nums[nums.length-1];
+	        int robNextPlusOne = 0;
+	        
+	        for(int i = nums.length - 2; i >= 0; i--){
+	            int cur = Math.max(robNext, robNextPlusOne + nums[i]);
+	            robNextPlusOne = robNext;
+	            robNext = cur;
+	        }
+	        
+	    return robNext;
+	    }
+	}
 
-    }
-}
 
 public class A4_House_Robber {
 
